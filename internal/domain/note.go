5829cdaeb15fc40a098d4a0c518f4cc077e5302e
jt00721 <titusgloverj@gmail.com>
2025-04-10 18:41:32 +0100

    feat: setup Note model and initialize GORM with Postgres

---
+++ internal/domain/note.go
@@ -0,0 +1,18 @@
+package domain
+
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
+
+type Note struct {
+	ID          uint   `gorm:"primaryKey"`
+	Title       string `gorm:"not null"`
+	Content     string `gorm:"not null"`
+	Category    string `gorm:"index"`
+	MeetingDate time.Time
+	CreatedAt   time.Time      `gorm:"autoCreateTime"`
+	UpdatedAt   time.Time      `gorm:"autoUpdateTime"`
+	DeletedAt   gorm.DeletedAt `gorm:"index"`
+}
