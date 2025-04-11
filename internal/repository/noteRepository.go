069c537784efeecec4d5a0c5c9529485c6185a60
jt00721 <titusgloverj@gmail.com>
2025-04-11 20:40:00 +0100

    Implement repository layer with CRUD Interface and methods

---
+++ internal/repository/noteRepository.go
@@ -0,0 +1,46 @@
+package repository
+
+import (
+	"github.com/jt00721/meeting-notes-manager/internal/domain"
+	"gorm.io/gorm"
+)
+
+type NoteRepository interface {
+	Create(n *domain.Note) error
+	GetAll() ([]domain.Note, error)
+	GetByID(id uint) (domain.Note, error)
+	Update(n *domain.Note) error
+	Delete(id uint) error
+}
+
+type noteRepository struct {
+	DB *gorm.DB
+}
+
+func NewNoteRepository(DB *gorm.DB) *noteRepository {
+	return &noteRepository{DB: DB}
+}
+
+func (r *noteRepository) Create(n *domain.Note) error {
+	return r.DB.Create(n).Error
+}
+
+func (r *noteRepository) GetAll() ([]domain.Note, error) {
+	var notes []domain.Note
+	err := r.DB.Find(&notes).Error
+	return notes, err
+}
+
+func (r *noteRepository) GetByID(id uint) (domain.Note, error) {
+	var note domain.Note
+	err := r.DB.First(&note, id).Error
+	return note, err
+}
+
+func (r *noteRepository) Update(n *domain.Note) error {
+	return r.DB.Save(n).Error
+}
+
+func (r *noteRepository) Delete(id uint) error {
+	return r.DB.Delete(&domain.Note{}, id).Error
+}
